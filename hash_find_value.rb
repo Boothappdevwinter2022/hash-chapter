f11d69adbd9b8abadfd507a930b1874a4270b6e4
thaong91 <tnguye13@chicagobooth.edu>
2022-01-24 04:25:49 +0000

    complete hw

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -4,6 +4,16 @@
 #   If you find the number, print out "100 is under the key: a."
 #   If you don't find the number print out "Could not find the integer 100"
 
-sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
+sample_hash = { :a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+
+user_number = gets.to_i
+
+number_key = sample_hash.index(user_number)
+
+if number_key != nil
+  p "#{user_number} is under the key: #{number_key}."
+else
+  p "Could not find the integer #{user_number}"
+end
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,8 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+
+user_inputs = gets.split
+
+person = { :name => user_inputs[0], :age => user_inputs[1].to_i, :occupation => user_inputs[2] }
+
+p person
